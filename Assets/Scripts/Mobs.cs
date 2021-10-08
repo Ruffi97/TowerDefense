ae7d6946baeb86ba63c7890cec0ac6f6b81a96c1
Ruffini <ruffinigeoffrey@gmail.com>
2021-10-08 12:59:11 +0200

    MobsMovement

--- Assets/Scripts/Mobs.cs
+++ Assets/Scripts/Mobs.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mobs : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject _goal;
+    private Vector3 _dest;
+    public NavMeshAgent agent;
     void Start()
     {
+        _goal = GameObject.FindGameObjectWithTag("goal");
+        _dest = _goal.transform.position;
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        agent.destination = _dest;
     }
+    
 }
